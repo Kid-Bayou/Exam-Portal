23f73904c6c3f66e4f28ebdfc91d805821064028
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-07-24 09:34:23 +0300

    general framework

---
+++ UI/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+function Footer() {
+    return (
+        <footer> 2022 - Exam Portal</footer>
+    )
+}
+
+export default Footer
---
+++ UI/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+function Home() {
+    return (
+        <>
+            <div className="home-container">
+                <h1>The Exam site for you</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
