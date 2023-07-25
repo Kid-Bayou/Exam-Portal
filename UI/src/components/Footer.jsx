141b4f6488b91ae49910a5e855893f16d789596e
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-07-25 10:46:29 +0300

    .

--- UI/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-function Footer() {
-    return (
-        <footer> 2022 - Exam Portal</footer>
-    )
-}
-
-export default Footer
--- UI/src/pages/Course/CreateCourse.jsx
+++
@@ -1,9 +0,0 @@
-function CreateCourse() {
-  return (
-    <>
-      <h1>this</h1>
-    </>
-  );
-}
-
-export default CreateCourse;
