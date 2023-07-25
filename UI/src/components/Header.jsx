b667ce229d54fa5dda2ae1e1a08df10eb8bfc318
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-07-25 11:55:05 +0300

    updates

--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -4,12 +4,12 @@ import logo from "../assets/logo.png"
 function Header() {
     return (
         <>
-            <header>
+            <header className="shadow-md flex py-4">
                 <Link className="site-logo" to="/">
-                    <img src={logo} className="logo" />
-                    <p className="logo-name">Exam Portal</p>
+                    <img src={logo} className="h-16 p-0 ml-3" />
+                    <p className="p-0 font-bold text-2xl mt-5 ml-3" >Exam Portal</p>
                 </Link>
-                <nav className="nav">
+                <nav>
                     <NavLink to="/courses">
                         Courses
                     </NavLink>
--- UI/src/pages/Course/CourseList.jsx
+++ UI/src/pages/Course/CourseList.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import axios from "axios";
 import { useState, useEffect } from "react";
 import {get} from "../../service/APIService"
 
@@ -38,7 +37,6 @@ function CourseList() {
         <div className="course-list">
           {courseElements}
         </div>
-
 {/*         <table>
           <thead>
             <td>Id</td>
