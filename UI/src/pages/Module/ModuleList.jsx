0d3df1381c31d28a2443f395a7a7a14dc75e4601
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-07-27 08:27:41 +0300

    a few more

--- UI/src/pages/Module/ModuleList.jsx
+++ UI/src/pages/Module/ModuleList.jsx
@@ -1,47 +1,43 @@
 import { Link } from "react-router-dom";
-import axios from "axios";
 import { useState, useEffect } from "react";
-import {get} from "../../service/APIService"
+import { get } from "../../service/APIService";
 
 function ModuleList() {
   const [module, setModule] = useState([]);
 
- const moduleElements = module.map((module) => (
-   <div key={module.id} className="grid mt-16">
-
-     <Link to={`/modules/${module.id}`}>
-       <div className="shadow-lg p-12 pl-16 pr-16">
-         <h3>{module.title}</h3>
-       </div>
-     </Link>
-     
-   </div>
+  const moduleElements = module.map((module) => (
+    <div key={module.id} className="module-tile">
+      <Link to={`/modules/${module.id}`}>
+        <div className="module-info">
+          <h3 className="module-info-text">{module.title}</h3>
+        </div>
+      </Link>
+    </div>
   ));
 
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
-        const responseData = await get("https://localhost:7182/api/Module/GetModules");
+        const responseData = await get(
+          "https://localhost:7182/api/Module/GetModules"
+        );
         setModule(responseData);
-      } catch(error) {
-        console.error('Error fetching data:', error);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
   }, []);
 
-
   return (
     <>
       <div className="module-list-container">
-
-        <div className="module-list">
-          {moduleElements}
-        </div>
+        <h2 className="module-header">Module</h2>
+        <div className="module-list">{moduleElements}</div>
       </div>
-      <div>
-      <Link to="/courses/createcourse">
-          Create Course
+      <div className="module-list-button">
+        <Link to="/courses/createcourse">
+          <button className="button">Create Module</button>
         </Link>
       </div>
     </>
