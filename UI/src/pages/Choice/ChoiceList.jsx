46e6148ec4bc058e0d368813fe9f54947654c93f
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-01 14:00:14 +0300

    updates

---
+++ UI/src/pages/Choice/ChoiceList.jsx
@@ -0,0 +1,9 @@
+function ChoiceList() {
+    return (
+        <>
+         <p>this</p>
+        </>
+    )
+}
+
+export default ChoiceList
--- UI/src/pages/Question/QuestionList.jsx
+++ UI/src/pages/Question/QuestionList.jsx
@@ -1,19 +1,20 @@
 import { Link, useParams } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { get } from "../../service/APIService";
-import {ExamContext} from "../../Context/ExamContext"
+import { ExamContext } from "../../Context/ExamContext";
+import ChoiceList from "../Choice/ChoiceList"
 
 function QuestionList() {
   const params = useParams();
   const { question, setQuestion } = useContext(ExamContext);
+  const [visibleChoices, setVisibleChoices] = useState({});
 
   const questionElements = question.map((question) => (
-    <div key={question.id} className="question-tile">
-      <Link to={`/questions/${question.id}`}>
-        <div className="question-info">
-          <h3 className="question-info-text">{question.questionContent}</h3>
-        </div>
-      </Link>
+    <div key={question.id} className="question-tile" onClick={() => toggleChoices(question.id)}>
+      <div className="question-info">
+        <h3 className="question-info-text">{question.questionContent}</h3>
+      </div>
+      {visibleChoices[question.id] && <ChoiceList />}
     </div>
   ));
 
@@ -31,16 +32,27 @@ function QuestionList() {
     fetchData();
   }, []);
 
+  const toggleChoices = (questionId) => {
+    setVisibleChoices((prevVisibleChoices) => ({
+      ...prevVisibleChoices,
+      [questionId]: !prevVisibleChoices[questionId],
+    }));
+  };
+
   return (
     <>
-        {question ? (
-      <div className="question-list-container">
-        <h2 className="question-header">Questions</h2>
-        <div className="question-list">{questionElements}</div>
+      {question ? (
+        <div className="question-list-container">
+          <h2 className="question-header">Questions</h2>
+          <div className="question-list">
+
+              {questionElements}
+
           </div>
-        ) : (
-          <h2>Loading</h2>
-        )}
+        </div>
+      ) : (
+        <h2>Loading</h2>
+      )}
 
       <div className="question-list-button">
         <Link to={`/questions/createquestion/${params.id}`}>
