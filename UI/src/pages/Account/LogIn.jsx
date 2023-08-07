10e74cce0e74bdb4b73081e73aab004a38d2826e
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-07 09:27:05 +0300

    log in

--- UI/src/pages/Account/LogIn.jsx
+++ UI/src/pages/Account/LogIn.jsx
@@ -1,33 +1,28 @@
+import {Link} from "react-router-dom"
+
 function Login() {
-    return (
-        <>
-            <h1 className="form-header">Login</h1>
-      <form className="form">
-        <label className="form-box">
-          <p className="form-label"> Email:</p>
-          <input
-          className="form-input"
-            type="text"
-            name="title"
-          />
-        </label>
-        <br />
-        <br />
-        <label className="form-box">
-          <p className="form-label"> Password:</p>
-          <input
-          className="form-input"
-            name="description"
-          />
-        </label>
-        <br />
-        <br />
-        <button className="button" type="submit">
-          Login
-        </button>
-      </form>
-        </>
-    )
+  return (
+    <>
+    <div className="sign-in-up">
+        <h1 className="sign-in-up-header">Login</h1>
+        <form className="sign-in-up-form">
+          <label className="sign-in-up-box">
+            <p className="sign-in-up-label"> Email:</p>
+            <input className="sign-in-up-input" name="description" />
+          </label>
+          <label className="sign-in-up-box">
+            <p className="sign-in-up-label"> Password:</p>
+            <input className="sign-in-up-input" name="description" />
+          </label>
+          <button className="button sign-in-up-btn" type="submit">
+            Login
+          </button>
+        </form>
+        <p className="sign-in-up-alternative">Don't Have An Account? <Link to="/" className="sign-in-up-other">Sign Up</Link></p>
+    </div>
+      
+    </>
+  );
 }
 
-export default Login
+export default Login;
