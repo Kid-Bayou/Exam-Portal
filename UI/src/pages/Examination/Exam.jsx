c954d97657fce0291dfa494bf46e897a6421aa43
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-14 15:44:04 +0300

    .

--- UI/src/pages/Examination/Exam.jsx
+++ UI/src/pages/Examination/Exam.jsx
@@ -1,14 +1,11 @@
-import { useState, useEffect } from "react";
 import Timer from "./Timer"
 
 function Exam() {
 
-  return (
-    <div className="App">
-      <h1>Animated Countdown Timer</h1>
-      <Timer initialTime={60} />
-    </div>
-  );
+    return(
+        <>
+            <Timer />
+        </>
+        )
 }
-
-export default Exam;
+export default Exam
--- UI/src/pages/Examination/timer.jsx
+++ UI/src/pages/Examination/timer.jsx
@@ -1,27 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-function Timer({ initialTime }) {
-  const [timeLeft, setTimeLeft] = useState(initialTime);
+const TIME_LIMIT = 7000; // Time in seconds
+
+function Timer() {
+  const [timeLeft, setTimeLeft] = useState(TIME_LIMIT);
 
   useEffect(() => {
     if (timeLeft > 0) {
-      const interval = setInterval(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
+      const timer = setInterval(() => {
+        setTimeLeft(timeLeft => timeLeft - 1);
       }, 1000);
-
-      return () => clearInterval(interval);
+      return () => clearInterval(timer);
     }
   }, [timeLeft]);
 
-  const formatTime = (timeInSeconds) => {
-    const minutes = Math.floor(timeInSeconds / 60);
-    const seconds = timeInSeconds % 60;
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  const calculatePath = (radius, progress) => {
+    const circumference = 2 * Math.PI * radius;
+    const offset = circumference - (progress / TIME_LIMIT) * circumference;
+    return { circumference, offset };
   };
 
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+    
+    const formattedTime = `${hours}:${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    return formattedTime;
+  };
+  
+
+  const { circumference, offset } = calculatePath(45, timeLeft);
+
   return (
-    <div className="countdown-timer">
-      <div className="timer">{formatTime(timeLeft)}</div>
+    <div className="circular-timer">
+      <svg className="circular-timer__svg" viewBox="0 0 100 100">
+        <circle
+          className="circular-timer__path-elapsed"
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="#ccc"
+          strokeWidth="6"
+          fill="transparent"
+        />
+        <circle
+          className="circular-timer__path-remaining"
+          cx="50"
+          cy="50"
+          r="45"
+          transform="rotate(-90 50 50)"
+          stroke="#ff5f5f"
+          strokeWidth="6"
+          fill="transparent"
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+        />
+        <text className="circular-timer__label" x="50" y="55">
+        {formatTime(timeLeft)}
+        </text>
+      </svg>
     </div>
   );
 }
