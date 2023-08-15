a85e58b4a7d207651004e4d120a090bc9cfa96e7
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-15 10:37:41 +0300

    .

---
+++ UI/src/pages/examTaker/Dashboard/Main.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar"
+
+function Main() {
+    return (
+        <>
+            <main className="App">
+            <Sidebar></Sidebar>
+            </main>
+        </>
+    )
+}
