2f431ac8b61f1008468481105cebab00694d93e1
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-16 14:41:40 +0300

    Notification page

---
+++ UI/src/pages/examTaker/pages/Notification.jsx
@@ -0,0 +1,11 @@
+function Notification() {
+    return (
+      <>
+        <div>
+          <h1>Welcome, Jane Doe to your Notifications!</h1>
+        </div>
+      </>
+    );
+  }
+  
+  export default Notification
