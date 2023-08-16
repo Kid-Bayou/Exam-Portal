2a0a7535bb80ea57db697d5c5ab04fb3facadb4d
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-16 14:40:30 +0300

    home page

---
+++ UI/src/pages/examTaker/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>Welcome, Jane Doe</h1>
+        <p>We're thrilled to have you here on our Exam Portal.</p>
+        <p>
+          Explore your personalized dashboard, where you can access your exams,
+          track your progress, and much more.
+        </p>
+        <p>
+          Get ready to embark on your learning journey and excel in your exams!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Home
