f418e10db4fb9b132e94e9d4c400aab86bf3c8a1
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-01 11:11:36 +0300

    something is working!

--- UI/src/pages/examTaker/examination/ExamHeader.jsx
+++ UI/src/pages/examTaker/examination/ExamHeader.jsx
@@ -2,33 +2,33 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { ExamContext } from "../../../context/ExamContext";
 import { API_BASE_URL, get, put } from "../../../service/APIService";
-import Timer from "./Timer"
+import Timer from "./Timer";
 
 function ExamHeader() {
-  
   const { exam, setExam } = useContext(ExamContext);
-  const [examination , setExamination] = useState(null)
+  const [examination, setExamination] = useState(null);
   const [newExam, setNewExam] = useState({
     id: "",
-      title: "",
-      startDateTime: "",
-      endDateTime: "",
-      moduleID: "",
-      examTakerID: "",
-  })
+    title: "",
+    startDateTime: "",
+    endDateTime: "",
+    moduleID: "",
+    examTakerID: "",
+  });
   const navigate = useNavigate();
   const currentDateTime = new Date();
 
-    
   useEffect(() => {
     fetchData();
   }, []);
-  
+
   const fetchData = async () => {
     try {
-      const responseData = await get(`${API_BASE_URL}/api/Examination/GetExaminationByStartDateAndTakerId?examStartDate=${exam.startDateTime}&examTakerId=${exam.examTakerID}`);
+      const responseData = await get(
+        `${API_BASE_URL}/api/Examination/GetExaminationByStartDateAndTakerId?examStartDate=${exam.startDateTime}&examTakerId=${exam.examTakerID}`
+      );
       setExamination(responseData);
-      console.log("me(getting the data is worksing")
+      console.log("me(getting the data is worksing");
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -36,23 +36,23 @@ function ExamHeader() {
 
   const handleEndExam = async (event) => {
     event.preventDefault();
-  
+
     console.log("woahhoashhhaoahaoojldsjfoasjg:", exam);
-  
+
     try {
-      console.log("when button is clicked", newExam)
-      console.log("when button is clicked", newExam.id)
-       const response = await put(
-         `${API_BASE_URL}/api/Examination/UpdateExamination/${newExam.id}`,
-         exam
-       );
-       navigate("examination/result/1")
-       console.log("end exam request successful:", response);
-     } catch (error) {
-       console.error("Error making put request:", error);
-     }
+      console.log("when button is clicked", newExam);
+      console.log("when button is clicked", newExam.id);
+      const response = await put(
+        `${API_BASE_URL}/api/Examination/UpdateExamination/${newExam.id}`,
+        newExam
+      );
+      navigate("/examination/result/1");
+      console.log("end exam request successful:", response);
+    } catch (error) {
+      console.error("Error making put request:", error);
+    }
   };
-  
+
   useEffect(() => {
     if (examination) {
       setNewExam((prevNewExam) => ({
@@ -64,21 +64,20 @@ function ExamHeader() {
         moduleID: exam.moduleID,
         examTakerID: exam.examTakerID,
       }));
-      console.log("i too am working!!!")
-      console.log("me?", newExam)
+      console.log("i too am working!!!");
+      console.log("me?", newExam);
     }
   }, [examination]);
-
-  
 
   return (
     <>
       <header className="examination-header">
-          <button className="e-button" onClick={handleEndExam}>End Exam</button>
-        <Timer  />
+        <button className="e-button" onClick={handleEndExam}>
+          End Exam
+        </button>
+        <Timer />
       </header>
     </>
   );
 }
-;
 export default ExamHeader;
